b3fd4e9e066ad82a76c78ade6c5fe703911de8e9
NEW <NEW>
2017-04-13 22:30:23 +0800

    首次提交

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /** \brief
+     *
+     * \param
+     * \param
+     * \return
+     *
+     */
+    /** \brief
+     *
+     * \param
+     * \param
+     * \return
+     *
+     */
+    /** \brief
+     *
+     * \param
+     * \param
+     * \return
+     *
+     */
+    /** \brief
+     *
+     * \param
+     * \param
+     * \return
+     *
+     */
+    cout << "Hello world!" << endl;/**< /**< /**< /**<  */ */ */ */
+    return 0;
+}
